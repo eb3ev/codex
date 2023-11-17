bee3b8ddc5687c52536bd9f1935ec036d2208305
eb3ev <ebv2905@gmail.com>
2023-11-17 15:49:32 +0000

    refactor: remove navigation pane border when hidden
    Navigation pane border is hidden when navigation pane is hidden.
    
    Additionally, cleanup and rearrange code.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,48 +1,6 @@
 let currPage = "pages/home.html"
-let navOpen = true
-
-function loadPage(page) {
-    sessionStorage.setItem("currPage", page);
-    $(document).ready(function() {
-        $("#page").load(page);
-    });
-}
-
-function reloadPage()
-{
-    if (sessionStorage.getItem("currPage")) {
-        let currPage = sessionStorage.getItem("currPage");
-
-        loadPage(currPage);
-    } else {
-        loadPage("pages/home.html")
-    }
-}
-
-function toggleNav() {
-    let ele = document.querySelector(":root");
-    let css = getComputedStyle(ele);
-
-    let nav = document.getElementById("nav");
-    let navWidth = css.getPropertyValue("--nav-width");
-
-    if (navOpen) {
-        nav.style.width = "0";
-        navOpen = !navOpen
-    } else {
-        nav.style.width = navWidth;
-        navOpen = !navOpen
-    }
-}
-
-const mql = window.matchMedia("screen and (max-width: 480px)");
-
-if (mql.matches) {
-    navOpen = false
-} else {
-    navOpen = true 
-}
-
+const mql = window.matchMedia("screen and (max-width: 680px)");
+let navOpen = (mql) ? false : true
 mql.addEventListener("change", function(e) {
     if (e.matches) {
         navOpen = false
@@ -59,4 +17,29 @@ window.addEventListener('resize', function() {
         prevWidth = window.innerWidth
     }
 }, true);
+
+function loadPage(page) {
+    $("#page").load(page);
+}
+
+function reloadPage() {
+    loadPage(currPage);
+}
+
+function toggleNav() {
+    let ele = document.querySelector(":root");
+    let css = getComputedStyle(ele);
+    let nav = document.getElementById("nav");
+    let navWidth = css.getPropertyValue("--nav-width");
+    let navBorderRight = css.getPropertyValue("--nav-border-right")
+    if (navOpen) {
+        nav.style.width = "0";
+        nav.style.border = "0";
+        navOpen = !navOpen;
+    } else {
+        nav.style.width = navWidth;
+        nav.style.borderRight = navBorderRight;
+        navOpen = !navOpen;
+    }
+}
 
