c53c91398a647ce2c693a840458c2518bdb9c0d8
eb3ev <ebv2905@gmail.com>
2023-11-17 03:23:07 +0000

    feat: remove header, use floating button instead
    Navigation pane is hidden on smaller screens and can be toggled using
    the new floating button which will also only appear on smaller screens.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -23,24 +23,15 @@ function toggleNav() {
     let nav = document.getElementById("nav");
     let navWidth = css.getPropertyValue("--nav-width");
 
-    let mainHeader = document.getElementById("main-header");
-    let page = document.getElementById("page");
-
     if (nav.style.width == "0px") {
         nav.style.width = navWidth;
-        mainHeader.style.marginLeft = navWidth;
-        page.style.marginLeft = navWidth;
     } else {
         nav.style.width = "0";
-        mainHeader.style.marginLeft = "0";
-        page.style.marginLeft = "0";
     }
 }
 
-function setDate() {
-    let date =  new Date();
-    let y = date.getFullYear();
-    let m = date.getMonth() + 1;
-    let d = date.getDate();
-    document.getElementById("date").innerHTML = d + " / " + m + " / " + y;
-}
+window.addEventListener('resize', function() {
+    let nav = document.getElementById("nav");
+    nav.style.width = "";
+}, true);
+
